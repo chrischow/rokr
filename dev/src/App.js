0327658d35492af9138110b2d90819134c7abbba
Chris Chow <christian.chow.en.yi@gmail.com>
2022-08-15 21:04:34 +0800

    Add NavBar component

--- dev/src/App.js
+++ dev/src/App.js
@@ -1,20 +1,23 @@
+import { HashRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
+// import TestQueries from './components/TestQueries/TestQueries';
+import NavBar from './components/NavBar/NavBar';
+import { config } from './config';
 import './App.css';
-import TestQueries from './components/TestQueries/TestQueries';
 
 function App() {
   // Load query client
   const queryClient = new QueryClient();
 
   return (
-    <>
+    <HashRouter>
       <QueryClientProvider client={queryClient}>
+        <NavBar teams={config.teams} />
         <div>
           <h1>Hello World!</h1>
-          <TestQueries />
         </div>
       </QueryClientProvider>
-    </>
+    </HashRouter>
   );
 }
 
---
+++ dev/src/components/NavBarBrand/NavBarBrand.js
@@ -0,0 +1,10 @@
+export default function NavBarBrand(){
+  return (
+      <div>
+          <span className='brand--navbar'>
+              <span className="accent-blue">R</span>
+              <span className="accent-green">OKR</span>
+          </span>
+      </div>
+  )
+}
