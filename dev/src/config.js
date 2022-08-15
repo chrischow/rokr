8b77128d8ad8a14634431e6fdbf279c19e57380b
Chris Chow <christian.chow.en.yi@gmail.com>
2022-08-15 19:43:31 +0800

    Add config file - initialised for Ravenpoint

---
+++ dev/src/config.js
@@ -0,0 +1,11 @@
+// Config file
+export const config = {
+  apiUrl: 'http://127.0.0.1:5000/ravenpoint/_api/',
+  objListId: '521d43a8e00c53c76ab48197784c5e41',
+  krListId: 'f3ef36940ef93890c3f9d9e25fc12acc',
+  updateListId: 'df7c2bf0cf2b0fda32b2998cbf8a07dc',
+  objListItemEntityTypeFullName: 'rokrObjectivesListItem',
+  krListItemEntityTypeFullName: 'rokrKeyResultsListItem',
+  updateListItemEntityTypeFullName: 'rokrUpdatesListItem',
+  staleTime: 2 * 60 * 1000
+};
