8ff27513616dc5593e54c4d01227dc2f35a2e6e2
Chris Chow <christian.chow.en.yi@gmail.com>
2022-08-16 11:42:48 +0800

    Add datatables imports

--- dev/src/index.js
+++ dev/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css';
 import 'bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js';
+import 'bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css';
+import 'datatables.net/js/jquery.dataTables.min';
+import 'datatables.net-bs4/js/dataTables.bootstrap4.min';
+import 'datatables.net-bs4/css/dataTables.bootstrap4.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
