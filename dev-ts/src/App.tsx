eec94fba04694f6feb8d7ec5aad88fb82d18745a
Chris Chow <christian.chow.en.yi@gmail.com>
2022-09-20 19:28:02 +0800

    Add ErrorBoundary, Home, Timeline, and Directory routes

--- dev-ts/src/App.tsx
+++ dev-ts/src/App.tsx
@@ -2,8 +2,11 @@ import { HashRouter, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import Container from "react-bootstrap/Container";
 import NavBar from './NavBar';
-// import Home from './Home';
+import ErrorBoundary from './ErrorBoundary';
+import Home from './Home';
 // import Team from './Team';
+import Timeline from './Timeline';
+import Directory from './Directory';
 // import Timeline from './Timeline';
 // import Directory from './Directory';
 // import Updates from './Updates';
@@ -26,7 +29,14 @@ function App() {
       <QueryClientProvider client={queryClient}>
         <NavBar teams={config.teams} />
         <Container className="mt-5 app-container">
-          
+          <ErrorBoundary>
+            <Routes>
+              <Route path="/" element={<Home teams={config.teams} />} />
+              <Route path="/timeline" element={<Timeline />} />
+              <Route path="/directory" element={<Directory />} />
+              {/* <Route path="/updates/:krId" element={<Updates />} /> */}
+            </Routes>
+          </ErrorBoundary>
         </Container>
       </QueryClientProvider>
     </HashRouter>
