f31ead936b1c3b534268a3785841655f181fd67e
Chris Chow <christian.chow.en.yi@gmail.com>
2022-09-20 14:15:45 +0800

    Prepare App and index

--- dev-ts/src/App.tsx
+++ dev-ts/src/App.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import Container from "react-bootstrap/Container";
+import NavBar from './NavBar';
+// import Home from './Home';
+// import Team from './Team';
+// import Timeline from './Timeline';
+// import Directory from './Directory';
+// import Updates from './Updates';
+// import ErrorBoundary from './shared/ErrorBoundary';
+import { config } from './config';
 import './App.css';
 
 function App() {
+  // Load query client
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: config.staleTime
+      }
+    }
+  });
+  
   return (
     <div>
     </div>
