d593455605a39d709b3c47fe28ae960fdca91c84
Chris Chow <christian.chow.en.yi@gmail.com>
2022-09-21 11:00:21 +0800

    Implement FreqDropdown

---
+++ dev-ts/src/Team/FreqDropdown/index.tsx
@@ -0,0 +1,41 @@
+import slugify from 'slugify';
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import './styles.css';
+
+interface FreqDropdownProps {
+  freq: string;
+  subgroups: string[];
+  options: [];
+  dateOption: string;
+  setDateOption: Function;
+}
+
+export default function FreqDropdown(props: FreqDropdownProps) {
+
+  // Handle updates
+  const handleChange = (event: any) => {
+    props.setDateOption(event.target.value);
+  }
+
+  // Populate options
+  const options = props.options.map(option => {
+    return <option key={`select-${props.freq}-${slugify(option)}`} value={option}>{option}</option>
+  });
+
+  return (
+    <Row className="justify-content-center mt-3">
+      <Col xs={3}>
+        <select
+          name={`subgroup-select-${props.freq}`}
+          className="form-control form-dark nav-subgroup-dropdown"
+          value={props.dateOption}
+          onChange={handleChange}
+        >
+          {options}
+        </select>
+      </Col>
+    </Row>
+  );
+}
