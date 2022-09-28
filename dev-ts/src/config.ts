466b930ef6ef3ce24b41f8ab8e2405ca78607260
Chris Chow <christian.chow.en.yi@gmail.com>
2022-09-28 21:17:04 +0800

    Add feedback list ID and list item entity type full name

--- dev-ts/src/config.ts
+++ dev-ts/src/config.ts
@@ -4,9 +4,11 @@ export const config = {
   objListId: '521d43a8e00c53c76ab48197784c5e41',
   krListId: 'f3ef36940ef93890c3f9d9e25fc12acc',
   updateListId: 'df7c2bf0cf2b0fda32b2998cbf8a07dc',
+  feedbackListId: '3db32ba6ff00becdf22961fe4cdec9e7',
   objListItemEntityTypeFullName: 'SP.Data.RokrObjectivesListItem',
   krListItemEntityTypeFullName: 'SP.Data.RokrKeyResultsListItem',
   updateListItemEntityTypeFullName: 'SP.Data.RokrUpdatesListItem',
+  feedbackListItemEntityTypeFullName: 'SP.Data.RokrFeedbackListItem',
   teams: [
     { teamName: "RAiD", slug: "raid" },
     { teamName: "PAB", slug: "pab" },
